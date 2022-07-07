20d54058bd155b9892398c081a3e2dda7b72c1c1
AlejandroIlgesias <93452718+AlejandroIlgesias@users.noreply.github.com>
2022-07-07 12:56:10 +0200

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -4,3 +4,5 @@ caramelos_manzana=0.5 #Precio por cada caramelo de manzana vacío
 unidades_manzanas=randint(10,20)#Cantidad de manzanas de caramelos vendidas
 unidades_caramelos=randint(1,200)#cantidad de caramelos vendidos
 if (unidades_manzanas%20)==0:
+  total_manzanas=(unidades_manzanas*precio_manzana)-(0.7*(unidades_manzanas//20))
+
