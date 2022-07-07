aff915730b9096de748b2a1e4161b4195f8a11ed
AlejandroIlgesias <93452718+AlejandroIlgesias@users.noreply.github.com>
2022-07-07 12:59:11 +0200

    Update Ejercicio 2.py

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -2,3 +2,4 @@ a=1#Valor de prueba
 def calcular(dollar_amount):
   dollar_value_euros=20514#precio en usd por bitcoin
   euros_value_usd=0.98#un dolar vale 0.98 euros
+  euros_value=(dollar_amount*dollar_value_euros)*euros_value_usd #Euros que tengo en bitcoin.
