64ab0145d6e007683bcf7b489291ea7778205663
AlejandroIlgesias <93452718+AlejandroIlgesias@users.noreply.github.com>
2022-07-07 12:55:28 +0200

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,3 +1,4 @@
 from random import randint
 precio_manzana=3.5 #Precio por cada manzana de caramelo
 caramelos_manzana=0.5 #Precio por cada caramelo de manzana vacío
+unidades_manzanas=randint(10,20)#Cantidad de manzanas de caramelos vendidas
