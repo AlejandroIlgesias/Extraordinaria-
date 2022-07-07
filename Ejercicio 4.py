cb0c2fc5ed3a880ec4ff546e7c43a71d10628ba2
AlejandroIlgesias <93452718+AlejandroIlgesias@users.noreply.github.com>
2022-07-07 13:05:15 +0200

    Update Ejercicio 4.py

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -8,3 +8,10 @@ def hexadecimal_a_binario(hexadecimal):
     binario=bstr
 
     return binario
+def binario_a_decimal(numero_binario):
+	numero_decimal = 0 
+
+	for posicion, digito_string in enumerate(numero_binario[::-1]):
+		numero_decimal += int(digito_string) * 2 ** posicion
+
+	return numero_decimal
